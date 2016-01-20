c3c6680336d2cbf91448b7e23680a74689ebb4ad
Noah <234082230@qq.com>
2016-01-20 11:26:09 +0800

    update FlurrySDK Unity side code

--- Assets/Sample/main.cs
+++ Assets/Sample/main.cs
@@ -1,28 +1,38 @@
 using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using Analytics;
 
 public class main : MonoBehaviour {
-#if UNITY_ANDROID
-	private string FLURRY_API = "S6NRW2DT77XG428PTD4V";
-#elif UNITY_IPHONE
-	private string FLURRY_API = "KRMJY5F2GPXXTBN65FR9";
-#else
-	private string FLURRY_API = "x";
-#endif
-	// Use this for initialization
+
+	private string FLURRY_API_ANDROID = "S6NRW2DT77XG428PTD4V";
+	private string FLURRY_API_IOS = "KRMJY5F2GPXXTBN65FR9";
+
 	void Start () {
-		FlurryAgent.Instance.onStartSession(FLURRY_API);
+		GameObject.Find("FlurryObject").GetComponent<Flurry>().StartSession(FLURRY_API_IOS, FLURRY_API_ANDROID);
+		GameObject.Find("FlurryObject").GetComponent<Flurry>().SetLogLevel(LogLevel.Debug);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (Time.frameCount % 200 == 0 || Time.frameCount < 2) {
-			FlurryAgent.Instance.logEvent ("test update");
-			Debug.Log ("logEvent test update");
+
+			// example for single log event
+			Debug.Log (GameObject.Find("FlurryObject").GetComponent<Flurry>().LogEvent("test new update"));
+
+			// example for log event with parameters
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+			parameters.Add("key", "value");
+			Debug.Log (GameObject.Find("FlurryObject").GetComponent<Flurry>().LogEvent("test with parameters", parameters));
+
+//			Debug.Log ("logEvent test update");
 		}
 	}
 	
 	void OnDestroy(){
-		FlurryAgent.Instance.onEndSession();
+
 	}
+
 }
